a6b2dcc3d90c5931d7341205d8e311698eccd7ba
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-23 02:51:23 +0530

    Create human.py

---
+++ WebStreamer/utils/human.py
@@ -0,0 +1,14 @@
+# This file is a part of TG-FileStreamBot
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: 'B', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n]
